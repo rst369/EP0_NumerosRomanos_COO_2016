05cba59a98aa8b20c223fc023284703791568215
Torres <Torres@Torres-PC.home>
2016-04-08 14:44:32 -0300

    versão funcional, testado até algarismo VII

--- Ep0_NumerosRomanos_coo16/src/Conversor.java
+++ Ep0_NumerosRomanos_coo16/src/Conversor.java
@@ -1,37 +1,53 @@
 
 public class Conversor {
 
-	public static String [] ordemRomanos ={"I"};
+	public static String [] ordemRomanos ={"V","IV","I"};
 
 	public static int deRomanoParaArabico(String numeroRomano){
 		int arabico =0;
-		
+		int anterior=0;
 		int tamanho=numeroRomano.length();
 		
+		System.out.println("romano: "+numeroRomano);
+		
 		for(int i=0;i<tamanho;i++){
-			arabico+=converteLetraRomanaNumero(numeroRomano.charAt(i)+"");
+			
+			int convertido = converteLetraRomanaNumero(numeroRomano.charAt(i)+"");
+			if(anterior==0 || convertido<=anterior)
+				arabico+= convertido;
+			else 
+				arabico= convertido-arabico;
+			
+			System.out.print("arabico: "+arabico+" convertido: "+convertido+" anterior: "+anterior);
+			
+			anterior = convertido;
 		}
-		
-		
+		System.out.println("arabico: "+arabico);
+		System.out.println("-------------------------------------------");
 		return arabico;
 	}
 	 public static String deArabicoParaRomano(int numeroArabico){
 		 String romano="";
-		 int potencia=1;
-		 
+		
 				
 				/**
 				 * pegar maior valor possivel para subtrair
 				 */
 		 	
-			
+//			System.out.println("arabico: "+numeroArabico);
 		 	while(numeroArabico>0){
 		 		int i=0;
 		 		while(i<ordemRomanos.length){
 					
 		 			String r = ordemRomanos[i];
-					numeroArabico -= converteLetraRomanaNumero(r);
-					romano+= ordemRomanos;
+		 			int nConvertido = deRomanoParaArabico(r);
+		 			if(nConvertido<=numeroArabico){
+		 				numeroArabico -= nConvertido; 
+						romano+= ordemRomanos[i];
+		 			}
+					
+					
+//					System.out.println(i+": r ="+r+" nConvertido="+nConvertido+" romano="+romano);
 					i++;
 				}
 		 	}
@@ -56,6 +72,7 @@ public class Conversor {
 		int a =0;
 		
 		if(r.equals("I")) return 1;
+		else if(r.equals("V")) return 5;
 		else return a;
 		
 	}
--- Ep0_NumerosRomanos_coo16/src/TestaConversorRomanos.java
+++ Ep0_NumerosRomanos_coo16/src/TestaConversorRomanos.java
@@ -35,5 +35,101 @@ public class TestaConversorRomanos {
 		String resultadoEsperado = "II";
 		assertEquals(resultadoEsperado, resultado);
 	}
+	
+	@Test
+	public void converte_III_para_3() {
+		
+		String romano = "III";
+		int resultadoEsperado = 3;
+		int resultado = Conversor.deRomanoParaArabico(romano);
+		
+		assertEquals(resultadoEsperado, resultado);
+	}
+	@Test
+	public void converte_3_para_III() {
+		
+		int arabico =3;
+		String resultadoEsperado = "III";
+		String resultado = Conversor.deArabicoParaRomano(arabico);
+		
+		assertEquals(resultadoEsperado, resultado);
+	}
+
+	
+	@Test
+	public void converte_IV_para_4() {
+		
+		String romano = "IV";
+		int resultadoEsperado = 4;
+		int resultado = Conversor.deRomanoParaArabico(romano);
+		
+		assertEquals(resultadoEsperado, resultado);
+	}
+	@Test
+	public void converte_4_para_IV() {
+		
+		int arabico =4;
+		String resultadoEsperado = "IV";
+		String resultado = Conversor.deArabicoParaRomano(arabico);
+		
+		assertEquals(resultadoEsperado, resultado);
+	}
+	
+	@Test
+	public void converte_V_para_5() {
+		
+		String romano = "V";
+		int resultadoEsperado = 5;
+		int resultado = Conversor.deRomanoParaArabico(romano);
+		
+		assertEquals(resultadoEsperado, resultado);
+	}
+	@Test
+	public void converte_5_para_V() {
+		
+		int arabico =5;
+		String resultadoEsperado = "V";
+		String resultado = Conversor.deArabicoParaRomano(arabico);
+		
+		assertEquals(resultadoEsperado, resultado);
+	}
+	
+	@Test
+	public void converte_VI_para_6() {
+		
+		String romano = "VI";
+		int resultadoEsperado = 6;
+		int resultado = Conversor.deRomanoParaArabico(romano);
+		
+		assertEquals(resultadoEsperado, resultado);
+	}
+	@Test
+	public void converte_6_para_VI() {
+		
+		int arabico =6;
+		String resultadoEsperado = "VI";
+		String resultado = Conversor.deArabicoParaRomano(arabico);
+		
+		assertEquals(resultadoEsperado, resultado);
+	}
+	
+	@Test
+	public void converte_VII_para_7() {
+		
+		String romano = "VII";
+		int resultadoEsperado = 7;
+		int resultado = Conversor.deRomanoParaArabico(romano);
+		
+		assertEquals(resultadoEsperado, resultado);
+	}
+	@Test
+	public void converte_7_para_VII() {
+		
+		int arabico =7;
+		String resultadoEsperado = "VII";
+		String resultado = Conversor.deArabicoParaRomano(arabico);
+		
+		assertEquals(resultadoEsperado, resultado);
+	}
 
 }
